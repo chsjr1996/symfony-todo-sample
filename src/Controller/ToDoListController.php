a8dbfcb5cc626fe5de1c38da4ffb2a45b9554541
Carlos Henrique <chsjr1996@gmail.com>
2022-05-01 13:18:25 -0300

    feat: prepare ToDoListController (WIP)
    - add some methods on ToDoListController
    - improve ToDoListController@index method adding a list of items
    - improve templates/index.html.twig template to use list of items

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -11,6 +11,37 @@ class ToDoListController extends AbstractController
     #[Route('/', name: 'todo_index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $todoListItems = [
+            ['id' => 1, 'title' => 'Study lua programming language', 'status' => 'undone'],
+            ['id' => 2, 'title' => 'Start study Symfony Framework', 'status' => 'done'],
+        ];
+
+        return $this->render('index.html.twig', [
+            'todoListItems' => $todoListItems
+        ]);
+    }
+
+    #[Route('/store', name: 'todo_store', methods: ['POST'])]
+    public function store(): Response
+    {
+        return $this->json([
+            'message' => 'Store todo... WIP',
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'todo_delete', methods: ['GET'])]
+    public function delete($id): Response
+    {
+        return $this->json([
+            'message' => "Delete todo ({$id})... WIP",
+        ]);
+    }
+
+    #[Route('/switch-status/{id}', name: 'todo_switch_status', methods: ['GET'])]
+    public function switchStatus($id): Response
+    {
+        return $this->json([
+            'message' => "Switch todo ({$id}) status... WIP",
+        ]);
     }
 }
